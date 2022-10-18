ec0fe96bfd1975a21765c74ca006187ec67813b1
maabdullah511 <maabdullah511@gmail.com>
2022-10-18 20:42:19 +0600

    install express and run server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-// server.js
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
