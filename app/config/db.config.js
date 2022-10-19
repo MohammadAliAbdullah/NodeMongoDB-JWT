b4360c7ccd89a3e5dff8cf01e5d775a614ceda89
maabdullah511 <maabdullah511@gmail.com>
2022-10-19 23:02:46 +0600

    auth apply

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,1 +1,9 @@
-// db config
+// module.exports = {
+//     url: "mongodb://localhost:27017/nodeMongodb-JWT"
+// };
+
+module.exports = {
+    HOST: "localhost",
+    PORT: 27017,
+    DB: "nodeMongodb-JWT",
+};
--- read_server.md
+++ read_server.md
@@ -71,5 +71,4 @@ then you will get `{"username": "dean"}`.
 
 
 
-
 source: https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
